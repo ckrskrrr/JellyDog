c0d0583665df4c5ab4896e8c28a3244bd0722798
ckrskrrr <ckr1370058527@gmail.com>
2025-11-17 23:31:45 -0500

    Refactor SQLITE_PATH to use BASE_DIR

--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
 import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    SQLITE_PATH = os.getenv("SQLITE_PATH", "databse.db")
+    SQLITE_PATH = os.getenv(
+        "SQLITE_PATH",
+        os.path.join(BASE_DIR, "database.db")
+    )
